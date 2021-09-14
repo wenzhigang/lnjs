cd2547f406664b00be5a978b385ae0a8abb5f444
zhigangwen <ares.wenzhigang@hotmail.com>
2021-09-14 23:33:51 +0800

    learnjs

---
+++ day8.js
@@ -0,0 +1,28 @@
+/*
+var num = 2;
+
+if(num == 1){
+    console.log("宜")
+} else if (num == 2){
+    console.log("二")
+}
+
+*/
+
+/*
+var num = 4;
+switch(num){
+    case 1:
+        console.log("宜")
+        break
+    case 2:
+        console.log("二")
+        break
+    case 3:
+        console.log("叁") 
+    default:
+        console.log("非法数字")
+}
+
+*/
+
